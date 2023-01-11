a820614e2c471ca3c03424467d9e781abd2d8078
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2023-01-11 10:26:43 -0500

    uploading project

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const {response}=require('express')
 const dataJson=require('../json/data.json')
 
@@ -24,10 +25,98 @@ const userPut=(req, res=response) => {
 }
 const userPost=(req, res) => {
 
-  const data=req.body;
+  const {govIssueIdentType,
+    identSerialNum}=req.body;
+    let data={};
+    if(identSerialNum==="1077870326"){
+      data={ "birthDt": "1995-05-27",
+      "cellPhone": "3209188638",
+      "emailAddr": "juanda5542@hotmail.com",
+      "addr1": "CL 69g#67-62",
+      "city": "11001",
+      "firstName": "Juan",
+      "isClient": true}
+    }else{
+      data={ "birthDt": "1986-06-29",
+      "cellPhone": "3209188632",
+      "emailAddr": "camilo@hotmail.com",
+      "addr1": "CL 89g#103-62",
+      "city": "11001",
+      "firstName": "Camilo",
+      "isClient": false}
+    }
   res.json(
-    dataJson
+    data
     );
+}
+const basicData=(req,res)=>{
+  return res.json({
+    "birthDt": "1995-05-27",
+    "cellPhone": "3209188638",
+    "emailAddr": "juanda5542@hotmail.com",
+    "addr1": "CL 69g#67-62",
+    "city": "11001",
+    "firstName": "Juan",
+    "isClient": true
+})
+}
+
+const userAnswer=(req,res)=>{
+  const {document_number}=req.body;
+  console.log(req.body)
+  let data;
+  if(document_number==="1018422010"){
+    data={
+      "step": "AUTH",
+      "key": "bea35950-c522-45aa-97ea-a183c4817cb5",
+      "type": "CLOSED-QUESTION",
+      "description": "Seleccione su número de celular",
+      "options": [
+          {
+              "id": "2d5e49c8-82ab-49d3-a042-ecc73d61b63f",
+              "option": "3142●●●●77"
+          },
+          {
+              "id": "fe84a00b-c0e5-48d2-9231-dd730e18ed00",
+              "option": "3183●●●●27"
+          },
+          {
+              "id": "9649a6ca-fc59-4d95-a01c-79b78117779a",
+              "option": "3142●●●●88"
+          },
+          {
+              "id": "f22021d8-0d1c-4cca-9c9e-dda7b691341e",
+              "option": "Ninguno"
+          }
+      ]
+  }
+  }else{
+    data={
+      "step": "VQ",
+      "key": "bea35950-c522-45aa-97ea-a183c4817cb5",
+      "type": "CLOSED-QUESTION",
+      "description": "Seleccione su número de celular",
+      "options": [
+          {
+              "id": "2d5e49c8-82ab-49d3-a042-ecc73d61b63f",
+              "option": "3142●●●●77"
+          },
+          {
+              "id": "fe84a00b-c0e5-48d2-9231-dd730e18ed00",
+              "option": "3183●●●●27"
+          },
+          {
+              "id": "9649a6ca-fc59-4d95-a01c-79b78117779a",
+              "option": "3142●●●●88"
+          },
+          {
+              "id": "f22021d8-0d1c-4cca-9c9e-dda7b691341e",
+              "option": "Ninguno"
+          }
+      ]
+  }
+  }
+  res.json(data)
 }
 const userDelete=(req, res) => {
   res.json(
@@ -42,5 +131,7 @@ module.exports ={
   userGet,
   userPut,
   userPost,
-  userDelete
+  userDelete,
+  userAnswer,
+  basicData
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,13 @@
 const {Router}=require('express');
-const { userGet, userPut, userPost, userDelete } = require('../controllers/user.controller');
+const { userGet, userPut, userPost, userDelete, userAnswer } = require('../controllers/user.controller');
 
 const router=Router();
 
 
 router.get('/', userGet);
 router.put('/:id', userPut);
-router.post('/', userPost);
+router.post('/basic-data', userPost);
+router.post('/answer', userAnswer);
 router.delete('/', userDelete);
 
 
